6e062a309267bc469e74a97b3a4c8a5335bfe3f9
EggsWithCheese <cdleong+github@gmail.com>
2015-03-05 00:08:11 -0500

    Comments and corrections
    Changed the format of some of the comments to python standard.
    Changed Equation 9 to use depth of 1% light instead.

--- mysite/pond.py
+++ mysite/pond.py
@@ -128,49 +128,68 @@ class Pond:
 
 
 
-    ######################
-    #Science functions
-    ######################
+    ####################################
+    # Science functions.
+    # Source: Vadeboncoeur et al, 2008
+    ####################################
 
-    #1 Lake area at depth z. Calculated from area at depth zero (the surface)
+
     def surfaceAreaAtDepthZ(self, z=0.0):
+        """
+        #1 Lake area at depth z. Calculated from area at depth zero (the surface)
+        """        
         if(z>self.getMaxDepth()):
             z=self.getMaxDepth()
         return self.surfaceAreaAtDepthZero*pow((1-(float(z)/self.maxDepth)),self.getShapeFactor())
     
-    #2 Lake volume above depth z.
-    # uses shape factor (gamma) 
+ 
     def lakeVolumeAboveDepthZ(self, z):
+        """
+        #2 Lake volume above depth z.
+        # uses shape factor (gamma)
+        """
         return (self.getShapeFactor()*z)/(self.getShapeFactor()+1)
-    
-    #3  phytoplankton chlorophyll Chl
-    # Calculated using magic numbers from total phosphorus
-    # source of magic: Prairie et al. (1989)
+
     def phytoplanktonChl(self):
+        """
+        # Equation 3:  phytoplankton chlorophyll Chl
+        # Calculated using magic numbers from total phosphorus
+        # source of magic: Prairie et al. (1989)\
+        """
         return self.getPhytoplanktoChlorophyll()
-    
-    #4 phytoplankton productivity, PP (mg C*m^-3*h^-1)
-    #source of magic: Guildford, et al (1994)
+
     def phytoplanktonProductivity(self):
+        """
+        # Equation 4: phytoplankton productivity, PP (mg C*m^-3*h^-1)
+        #source of magic: Guildford, et al (1994)
+        """
         return 2.2*self.getPhytoplanktoChlorophyll() #magic
+
     
-    #5 thermocline depth (m) 
-    #calculated from surface area using magic numbers 
-    #from paper (Hanna 1990)    
     def thermoclineDepth(self):
+        """    
+        # Equation 5 thermocline depth (m) 
+        #calculated from surface area using magic numbers 
+        #from paper (Hanna 1990)
+        """
         return 6.95*pow(self.surfaceAreaAtDepthZero,0.185) #magic
     
-    #6 light-attenuation coefficient (m^-1)
-    #calculated from #3, 
-    #or, in real life, just measured. 
-    #source: Light attenuation and photosynthesis of aquatic plant communities 
-    # Krause-Jensen and Sand-Jensen, 1998
+
     def calculateLightAttenuationCoefficient(self):
+        """
+        # Equation 6 light-attenuation coefficient (m^-1) "kd"
+        #calculated from #3, 
+        #or, in real life, just measured. 
+        #source: Light attenuation and photosynthesis of aquatic plant communities 
+        # Krause-Jensen and Sand-Jensen, 1998
+        """
         return self.getBackgroundLightAttenuation()+0.015*self.getPhytoplanktoChlorophyll()
-    
-    #7 light at depth z, time t (umoles*m^-2*s^-1)
-    #calculated from light attenuation factor, surface light at time 0, depth.
+
     def lightAtDepthZAndTimeT(self, depths=None, t=6.0): #if day length 12, 6 is noon.
+        """
+        #7 light at depth z, time t (umoles*m^-2*s^-1)
+        #calculated from light attenuation factor, surface light at time 0, depth.
+        """        
         if depths is None:           
             depths = []
             depths.append(0.0)
@@ -192,15 +211,20 @@ class Pond:
             return lightValues[0]
         else:
             return lightValues
-    
-    #8 surface light at time t (umoles*m^-2*s^-1)
-    #source of equation: McBride (1992)
+
     def surfaceLightAtTimeT(self, t=6.0): #if day length 12, 6 is noon.
+        """
+        #8 surface light at time t (umoles*m^-2*s^-1)
+        #source of equation: McBride (1992)
+        """        
         return self.noonLight*np.sin((np.pi*(t/self.dayLength)))
     
-    #9 daily phytoplankton PP at depth Z (mg C)
-    #summed from sunrise to sunset.
+
     def dailyPPatDepthZ(self,deltaT=0.25,deltaZ=0.1, saturationLight=180, depths=None):
+        """
+        #9 daily phytoplankton PP at depth Z (mg C)
+        #summed from sunrise to sunset.
+        """        
         if depths is None:           
             depths = []
             depths.append(0.0)
@@ -231,20 +255,25 @@ class Pond:
        
 
 
-    #10 daily whole-lake phytoplankton production, TPP (mg C/m^2)
-    #basically #9, summed from depth 0 to depth epi (epilimnion), 
-    #then that quantity divided by surface area at zero.
     def dailyTPP(self, deltaT=0.25, deltaZ=0.1, saturationLight=180):
+        """
+        # Equation 10: daily whole-lake phytoplankton production, TPP (mg C/m^2)
+        #basically #9, summed from depth 0 to of 1% surface light. 
+        #then that quantity divided by surface area at zero.
+        """
         summation = 0.0
         z=0.0 #lake surface
-        while(z<self.maxDepth):
+        z1percent=4.6/self.calculateLightAttenuationCoefficient()#depth of 1%surfaceLight
+        while(z<z1percent):
             z+=deltaZ
             summation +=self.dailyPPatDepthZ(deltaT, deltaZ, saturationLight,z)
         return summation/self.surfaceAreaAtDepthZero
 
 
-    #11 daily benthic primary production, BP, at depth z (mg C)
     def dailyBPatDepthZ(self,deltaT=0.25, deltaZ=0.1, saturationLight = 300, z=0):
+        """
+        #11 daily benthic primary production, BP, at depth z (mg C)
+        """        
         summation = 0.0
         bpMax = self.getBPMax()
         areaAtZ = self.surfaceAreaAtDepthZ(z)
@@ -257,8 +286,11 @@ class Pond:
             summation += bpMax*hyperbolicThing*deltaArea
         return summation
     
-    #12 daily whole-lake periphyton production, TBP (mg C/m2 )
-    def dailyTBP(self, deltaT=0.25, deltaZ=0.1, saturationLight = 300):
+
+    def dailyTotalBenthicProduction(self, deltaT=0.25, deltaZ=0.1, saturationLight = 300):
+        """
+        #12 daily whole-lake periphyton (AKA, Benthic) primary production, TBP (mg C/m2 )
+        """
         summation = 0.0
         z=0.0 #lake surface
         while(z<self.getMaxDepth()):
