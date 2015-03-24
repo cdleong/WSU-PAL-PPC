521368b83b5d486b8263720178622f5a10e1c806
EggsWithCheese <cdleong+github@gmail.com>
2015-03-24 00:04:32 -0400

    Almost done replicating SAS code
    TODO: flesh out PondLayer
    fractional area
    BPPRZT
    BPPRZT/4
    Opt_Bppr

---
+++ mysite/PondLayer.py
@@ -0,0 +1,30 @@
+'''
+Created on Mar 23, 2015
+
+@author: cdleong
+'''
+
+
+class PondLayerMeasurement(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        KNOWS
+        '''
+        area = 0
+        ik = 0
+        pmax = 0 
+        z = 0
+        
+        
+        
+        
+        
+        '''
+        Constructor
+        '''
+        
--- mysite/data_reader.py
+++ mysite/data_reader.py
@@ -4,6 +4,7 @@ Created on Mar 5, 2015
 @author: cdleong
 '''
 import xlrd, xlwt #reading and writing, respectively. 
+from mysite import PondLayer
 
 
 #Useful notes: http://www.youlikeprogramming.com/2012/03/examples-reading-excel-xls-documents-using-pythons-xlrd/
@@ -51,8 +52,11 @@ def main():
     #I successfully printed a row. I have discovered how to read data from excel spreadsheets.
     #success
     
+    #############################################
+    #Keep only one lake
+    #############################################
     
-    #now to keep only the rows for, say, one lake. 
+     
     #how
     curr_row=1
     rowData = workSheet.row(curr_row)
@@ -65,21 +69,104 @@ def main():
     #print xlrd.colname(2) #gives "C"
     
     #test. 
-    rowsThatHaveUS_Spark = []
-    curr_row = -1
+    rowsForOneLake = []
+    curr_row = 0
     while curr_row<num_rows:
         curr_row+=1
         row = workSheet.row(curr_row)
         #print "the value at row index ", lakeIDIndex, " is ", row[lakeIDIndex] 
         if ('US_SPARK'==row[lakeIDIndex].value):
-            rowsThatHaveUS_Spark.append(row)
+            rowsForOneLake.append(row)
             
-    print len(rowsThatHaveUS_Spark)   # 1120 rows, or, about a third of them. Not unreasonable 
-    print rowsThatHaveUS_Spark[0] #hey it worked.
+    print len(rowsForOneLake)   # 1120 rows, or, about a third of them. Not unreasonable 
+    print rowsForOneLake[0] #hey it worked.
     
+    
+    #################################################
+    #Keep only one DOY
+    #################################################
+    dayOfYearIndex = 1
+    curr_column = dayOfYearIndex
+    curr_row = 0
+    doy = rowsForOneLake[curr_row][curr_column].value
+    print "DAY OF YEAR: ", doy
+    
+    
+    rowsForOneLakeAndOneDay = []
+    while curr_row<len(rowsForOneLake)-1:
+        curr_row+=1
+        row = rowsForOneLake[curr_row]
+        
+        if (doy==row[curr_column].value):
+            print "the value at row index ", curr_column, " is ", row[curr_column] 
+            rowsForOneLakeAndOneDay.append(row)
+    
+
+    
+
+    
+    ##########################
+    #Keep only littoral area
+    ##########################
     #find kd
-    curr_column = 14 #I think it is this one
+    kd_index = 14 #I think it is this one
+    #print columnnames[kd_index] #yup
+    
+    #find depth
+    depth_index = 3
+#     print columnnames[depth_index] #yup
+    
+    
+    #depth of 1% light is 4.6/kd
+    
+    #kd is the same for the whole lake for one day.
+    curr_row = 0
+    curr_column = kd_index
+    kd = rowsForOneLakeAndOneDay[curr_row][curr_column].value
+    print "THE KD VALUE IS", kd
+    
+    d1percent = 4.6/kd #depth of 1%light, meters.
+    print "depth of 1%light is ", d1percent
+    
+    
+    curr_row = 0
+    curr_column = depth_index
+    rowsForLittoralArea = []
+    while curr_row<len(rowsForOneLakeAndOneDay)-1:
+        curr_row+=1
+        row = rowsForOneLakeAndOneDay[curr_row]
+         
+        if (d1percent>row[curr_column].value):
+            print "the value at row index ", curr_column, " is ", row[curr_column] 
+            rowsForLittoralArea.append(row)    
+            
+    ###############################
+    #Calculate total littoral area
+    ###############################
+    area_index = 16
+    curr_column = area_index
     print columnnames[curr_column] #yup
+    
+    
+    
+    
+    curr_row = 0
+    curr_column = area_index
+    total_area = 0 #I expect 518220
+    while curr_row<len(rowsForLittoralArea)-1:
+        curr_row+=1
+        row = rowsForLittoralArea[curr_row]
+        area_at_z = row[curr_column].value
+        total_area += area_at_z
+        print "area at ", row[depth_index].value, " is ", row[area_index].value
+        
+    
+    print "TOTAL AREA: ", total_area
+        
+    
+    
+    
+    
     
   
     
